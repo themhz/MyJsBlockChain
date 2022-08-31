91d67050f1f77ed68c8e61af71296c3174970b36
themhz <themhz@gmail.com>
2022-08-31 12:55:54 +0300

    first commit

--- main.js
+++ main.js
@@ -7,16 +7,27 @@ class Block {
       this.timestamp = timestamp;
       this.data = data;
       this.hash  = this.calculateHash();
+      this.nonce = 0;
     }
 
     calculateHash(){
-      return SHA256(this.index+this.previousHash+this.timestamp+JSON.stringify(this.data)).toString();
+      return SHA256(this.index+this.previousHash+this.timestamp+JSON.stringify(this.data) + nonce).toString();
+    }
+
+    mineBlock(difficulty){
+        while(this.hash.substring(0, difficulty)!== Array(difficulty+1).join("0")){
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+
+        console.log("Block mined: " + this.hash);
     }
 }
 
 class BlockChain{
     constructor(){
       this.chain = [this.createGenesisBlock()];
+      this.difficulty = 2;
     }
     createGenesisBlock(){
       return new Block(0, "01/01/2017", "Genesis block", "0");
@@ -28,7 +39,8 @@ class BlockChain{
 
     addBlock(newBlock){
       newBlock.previousHash = this.getLatestBlock().hash;
-      newBlock.hash = newBlock.calculateHash();
+    //   newBlock.hash = newBlock.calculateHash();
+      newBlock.mineBlock(this.difficulty);
       this.chain.push(newBlock);
     }
 
@@ -61,5 +73,5 @@ codeCraftCoin.addBlock(new Block(1, "12/07/2017",{amount:10}));
 
 console.log(codeCraftCoin.chain);
 
-codeCraftCoin.chain[1].data.amount = 4;
+
 console.log(codeCraftCoin.isChainValid());
